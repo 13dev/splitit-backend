ae2d263062b0b590dbb3d23d0175b2c75b8507a6
13dev <qwerty124563@gmail.com>
2020-04-01 22:52:09 +0100

    fix .gitlab-ci.yml

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return [
-        'app' => 'Laravel 6 - ' . env('APP_NAME'),
+        'app' => 'Laravel 6 "' . env('APP_NAME') . '"',
         'version' => config('api.version'),
     ];
 });
